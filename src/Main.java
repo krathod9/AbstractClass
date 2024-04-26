bfc2cfcc752875cdf24a7a8a8428efa315f88de2
krnra <karan.rathod099@gmai.com>
2024-04-26 21:57:10 +0530

    test for abstract class

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         Circle c=new Circle("Red",5);
-        System.out.println(c.info());
+        System.out.println(c.area());
 
         Square s=new Square("blue",3);
         System.out.println(s.info());
